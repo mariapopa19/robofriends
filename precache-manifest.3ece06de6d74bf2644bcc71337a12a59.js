6adf614f232cc4afdecee8811f861409c3f29543
Maria Popa <maria_olimp@yahoo.com>
2022-07-27 17:30:16 +0300

    Updates

---
+++ precache-manifest.3ece06de6d74bf2644bcc71337a12a59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "adec00563b05fdeab81d",
+    "url": "/robofriends/static/css/main.866051c0.chunk.css"
+  },
+  {
+    "revision": "adec00563b05fdeab81d",
+    "url": "/robofriends/static/js/main.adec0056.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "395e82a10e5ccfdf97ba",
+    "url": "/robofriends/static/css/2.18b6ac06.chunk.css"
+  },
+  {
+    "revision": "395e82a10e5ccfdf97ba",
+    "url": "/robofriends/static/js/2.395e82a1.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d53d9abca02c5cf55a2a661a226fcc5b",
+    "url": "/robofriends/index.html"
+  }
+];
